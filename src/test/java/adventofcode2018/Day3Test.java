8f0fdde7e8d94bb71980e7abca31f41ea13bf404
unknown <mdrubiop@gmail.com>
2019-01-27 10:17:15 +0100

    New test

---
+++ src/test/java/adventofcode2018/Day3Test.java
@@ -0,0 +1,21 @@
+package adventofcode2018;
+
+import static org.junit.Assert.assertEquals;
+
+import java.util.regex.Pattern;
+
+import org.junit.Test;
+
+public class Day3Test {
+
+	@Test
+	public void info_by_one_claim() {
+		
+		String claim = "#123 @ 3,2: 5x4";
+		String[] infoClaim = claim.split("#\\@\\:\\x");
+		
+		assertEquals("123", infoClaim[0]);
+		
+	}
+
+}
