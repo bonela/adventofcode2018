d57cec6dc136248e9df2175fbbef3a3efd684201
Lola Rubio <36568004+bonela@users.noreply.github.com>
2019-01-27 10:24:53 +0100

    removing code test

--- src/test/java/adventofcode2018/Day3Test.java
+++ src/test/java/adventofcode2018/Day3Test.java
@@ -12,8 +12,7 @@ public class Day3Test {
 	public void info_by_one_claim() {
 		
 		String claim = "#123 @ 3,2: 5x4";
-		String[] infoClaim = claim.split("#\\@\\:\\x");
-		System.out.printl("Only a test to git");
+		String[] infoClaim = claim.split("#\\@\\:\\x"); 
 		assertEquals("123", infoClaim[0]);
 		
 	}
